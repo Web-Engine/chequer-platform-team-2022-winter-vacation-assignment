eaf7f085238ebce9b91a5c82009161aac87bdb59
Wein Cho <wein@chequer.io>
2023-01-02 04:25:07 +0900

    Use SqlExecutor in CsvLite.Cli

--- CsvLite.Cli/Program.cs
+++ CsvLite.Cli/Program.cs
@@ -13,96 +13,19 @@ namespace CsvLite.Cli;
 
 public class Program
 {
-    // public static void Main(string[] args)
-    // {
-    //     using var streamReader = new StreamReader("datasets/freshman_kgs.csv");
-    //     using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
-    //
-    //     csvReader.Read();
-    //     csvReader.ReadHeader();
-    //
-    //     var headerRecords = csvReader.HeaderRecord ?? ArraySegment<string>.Empty;
-    //     Console.WriteLine(string.Join(",", headerRecords));
-    //
-    //     while (csvReader.Read())
-    //     {
-    //         for (var i = 0; i < headerRecords.Count; i++)
-    //         {
-    //             Console.Write(csvReader.GetField(i));
-    //             Console.Write(",\t");
-    //         }
-    //
-    //         Console.WriteLine();
-    //     }
-    //
-    //     Console.WriteLine();
-    // }
-
-//     public static void Main(string[] args)
-//     {
-//         var relationPresenter = new RelationPresenter(Console.Out);
-//         var parser = new SqlParser();
-//
-// //         const string sql = @"
-// // SELECT COUNT(*), Sex FROM ""datasets/freshman_lbs.csv"" as kgs GROUP BY ""Sex""
-// // ";
-//
-//         const string sql = @"
-//             SELECT
-//                 *
-//             FROM
-//                 ""datasets/freshman_kgs.csv"" as kgs,
-//                 ""datasets/freshman_lbs.csv"" as lbs
-//             WHERE
-//                 (
-//                     SELECT
-//                          COUNT(*)
-//                      FROM
-//                         ""datasets/freshman_kgs.csv"" d1,
-//                         ""datasets/freshman_kgs.csv"" d2,
-//                         ""datasets/freshman_kgs.csv"" d3
-//                 ) OR TRUE
-//             LIMIT 10
-// ";
-//
-//         var provider = new CsvRelationProvider();
-//         var engine = new SqlEngine(provider);
-//
-//         var optimizer = new RuleBasedOptimizer();
-//
-//         var action = parser.Parse(sql);
-//         action = optimizer.Optimize(action);
-//         
-//         var result = engine.Execute(action);
-//
-//         if (result is IRelationResult {Relation: var relation})
-//         {
-//             relationPresenter.Show(relation);
-//         }
-//
-//         if (result is IAppendRecordResult { Count: var count})
-//         {
-//             Console.WriteLine($"{count} Records Inserted");
-//         }
-//
-//         Console.WriteLine($"Time elapsed: {result.Elapsed}");
-//
-// }
-
     public static void Main(string[] args)
     {
         var relationProvider = new CsvRelationProvider();
-        var sqlEngine = new SqlEngine(relationProvider);
-        var parser = new SqlParser();
-        var optimizer = new RuleBasedOptimizer();
-        var relationPresenter = new RelationTextWriter(Console.Out);
+        var executor = new SqlExecutor(relationProvider);
+
+        var relationTextWriter = new RelationTextWriter(Console.Out);
 
         while (true)
         {
             Console.Write("CQL> ");
 
-            var sql = Console.ReadLine();
-            if (sql is null || sql.Trim().Replace(";", "").Equals("exit"))
+            var sql = Console.ReadLine()?.Trim();
+            if (sql is null || $"{sql};".StartsWith("exit;", StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("Goodbye");
                 break;
@@ -110,14 +33,10 @@ public class Program
 
             try
             {
-                var action = parser.Parse(sql);
-                action = optimizer.Optimize(action);
-
-                var result = sqlEngine.Execute(action);
+                var result = executor.Execute(sql);
 
                 if (result is IRelationResult relationResult)
-                    relationPresenter.Write(relationResult.Relation);
-
+                    relationTextWriter.Write(relationResult.Relation);
                 else if (result is IAppendRecordsResult appendRecordResult)
                     Console.WriteLine($"{appendRecordResult.Count} Rows Inserted");
 
